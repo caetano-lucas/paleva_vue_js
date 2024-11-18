4ba6f6b7a3ceeed80425c79cb94da3d0050b4776
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-11-18 19:44:15 -0300

    adiciona lista e ordena pelos mais antigos.

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            clientName: 'Lucas',
-            orderTime: '08:18h 18/11',
-            orderStatus: 'Aguardando confirmação cozinha',
-
-            ordemItemsList: [
-                {
-                    PortionName: '12312',
-                    PortionableName: 'aaaaa',
-                    Note: 'bbbbbbbb',
-                    Quantity: 12
-                }
-            ]
-        }
+    data() {
+      return {
+        
+        orders: [],
+        ordemItemsList: [],
+        clientName: '',
+        orderTime: '',
+        orderStatus: '',
+        
+        showList: true,
+        showDetails: false,
+      };
     },
-    methods:{
-        changeStatus(){
-            this.orderStatus = 'preparação'
-        },
-
-        async getData() {
-            let response = await fetch('http://localhost:3000/api/v1/restaurants/11bs0k/orders/ou5634n4');
-           
-              let data = await response.json();
-              console.log(data);
-              
-              this.orderCode = data.order.alphanumeric_code
-              this.clientName = data.order.client_name
-              this.orderTime = data.order.created_at
-              this.orderStatus = data.order.status
-              this.ordemItemsList = [];
-              data.order_items.forEach(order_item => {
-                const newItem = {
-                    PortionName: order_item.portion_name,
-                    PortionableName: order_item.portion_description,
-                    Note: order_item.note,
-                    Quantity: order_item.quantity
-                };
-                this.ordemItemsList.push(newItem);
-            });
-        }
+    methods: {
       
-    }
-
-})
-
-app.mount('#app')
+      goBack() {
+        this.showList = true;
+        this.showDetails = false;
+      },
+      
+      async getData() {
+        const response = await fetch('http://localhost:3000/api/v1/restaurants/11bs0k/orders/', {
+          cache: 'no-store',
+        });
+        const data = await response.json();
+        this.orders = data.orders
+        .filter(item => item.status === 'aguardando_confirmacao_cozinha' || item.status === 'preparação')
+        .map(order => ({
+          orderCode: order.alphanumeric_code,
+          orderClientName: order.client_name,
+          orderTime: order.created_at,
+          orderStatus: order.status,
+        }));
+        this.showList = true;
+        this.showDetails = false;
+      },
+      
+      async getOrder(orderCode) {
+        const response = await fetch(`http://localhost:3000/api/v1/restaurants/11bs0k/orders/${orderCode}`);
+        const data = await response.json();
+        this.clientName = data.order.client_name;
+        this.orderTime = data.order.created_at;
+        this.orderStatus = data.order.status;
+        this.ordemItemsList = data.order_items.map(item => ({
+          PortionName: item.portion_name,
+          PortionableName: item.portion_description,
+          Note: item.note,
+          Quantity: item.quantity,
+        }));
+        this.showList = false;
+        this.showDetails = true;
+      },
+    },
+  });
+  
+  app.mount('#app');
